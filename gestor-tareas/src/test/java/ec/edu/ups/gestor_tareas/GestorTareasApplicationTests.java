eea4cdb8d84f37108524175e79aa0498be38234b
dario <djfalconi@hotmail.com>
2024-12-07 09:41:21 -0500

    Creación del proyecto base

---
+++ gestor-tareas/src/test/java/ec/edu/ups/gestor_tareas/GestorTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.gestor_tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
