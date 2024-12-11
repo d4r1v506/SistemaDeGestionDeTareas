a7b91e04b0c48d6c1f71f68c32c1cd29bcab23ad
Darius <dfalconi@praxmed.com.ec>
2024-12-11 17:37:03 -0500

    cambio de metodo put a patch en el api actualizar estado tarea

--- gestor-tareas/src/main/java/ec/edu/ups/gestor_tareas/controllers/TareaController.java
+++ gestor-tareas/src/main/java/ec/edu/ups/gestor_tareas/controllers/TareaController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -76,7 +77,7 @@ public class TareaController {
 		}
 	}
 
-	@PutMapping("/{id}")
+	@PatchMapping("/{id}")
 	public ResponseEntity<?> actualizarEstadoTarea(@PathVariable Long id, @RequestBody Estado estadoRequest) {
 
 		try {
